7d7866fd69224fe44be808d6b705487edd71213e
jainchintu623 <jainchintu@623gmail.com>
2023-12-28 23:00:01 +0530

    alert box enable

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
 
 import './App.css';
-import About from './Components/About';
+// import About from './Components/About';
 import Navbar from './Components/Navbar';
 
+import TextForm from './Components/TextForm';
+import Alert from './Components/Alert';
+import React, {useState}  from 'react'
 
-// import TextForm from './Components/TextForm';
+
 function App() {
+const [mode, setMode]=useState('light');
+const [grayMode, setGrayMode]=useState('light');
+
+const [alert, setAlert]=useState(null);
+
+const showAlert = (message,type)=>
+{
+  setAlert({
+    msg:message,
+    type:type
+  })
+  setTimeout(() => {
+     setAlert(null)
+  }, 1500);
+}
+
+const toggleMode=()=>{
+  if(mode==='light'){
+    setMode('dark');
+    document.body.style.backgroundColor='#042743';
+    showAlert ("Dark mode has been enbled", "success")
+    document.title ='Textutils -Dark Mode Enable'
+  }
+  else{
+    setMode('light');
+    document.body.style.backgroundColor='white';
+    showAlert ("Light mode has been enbled", "success")
+    document.title ='Textutils -light Mode Enable'
+  }
+}
+const toggleModeGray=()=>{
+  if(grayMode==='light'){
+    setGrayMode('gray');
+    document.body.style.backgroundColor='red';
+    showAlert ("Dark mode has been enbled", "success")
+  }
+  else{
+    setGrayMode('light');
+    document.body.style.backgroundColor='white';
+    showAlert ("Light mode has been enbled", "success")
+  }
+}
   return (
     <>
    
-<Navbar tittle= "TextUtils" AboutText= "About Us"/>
+<Navbar tittle= "TextUtils"  mode={mode} toggleMode={toggleMode}  grayMode={grayMode} toggleModeGray={toggleModeGray}/>
+<Alert alert={alert}/>
 <div className="container p-3">
-{/* <TextForm heading="Enter The text "/> */}
-<About/>
+<TextForm   showAlert={ showAlert}  heading="Enter The text "  mode={mode} grayMode={grayMode}/>
 </div>
 </>
   );
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -6,17 +6,20 @@ export default function TextForm(props) {
     // console.log("uppercase was clicked" + text);
     let newText = text.toUpperCase();
      setText(newText);
+     props.showAlert("Convert to the Uppercase","success")
 }
 
 const handleLoClick =()=>{
   // console.log("uppercase was clicked" + text);
   let newText = text.toLocaleLowerCase();
    setText(newText);
+   props.showAlert("Convert to the Lowercase", "success")
 }
 const handleClearClick =()=>{
   // console.log("uppercase was clicked" + text);
   let newText = '';
    setText(newText);
+   props.showAlert("Text clear", "success")
 }
 
 
@@ -27,31 +30,61 @@ const handleClearClick =()=>{
         //    console.log("onchange");
            setText(event.target.value)
     }
+
+    const handleCopyClick=()=>{
+      let text =document.getElementById("exampleFormControlTextarea1");
+      text.select();
+      navigator.clipboard.writeText(text.value);
+      props.showAlert("copy clipboard", "success")
+    }
+
+    const handleExtraSpaceClick =()=>{
+      let newText =text.split(/[ ]+/);
+      setText(newText.join(" "))
+      props.showAlert("Remove Extra Space", "success")
+    }
+
     const [text, setText] = useState("");
 
   return (
     <>
-    <div>
+    <div className='conatiner' style={{backgroundColor: props.mode==='dark'?'#042743':'white', color: props.mode==='dark'?'white':'#042743'},
+        {backgroundColor: props.grayMode==='gray'?'red':'white', color: props.grayMode==='gray'?'white':'black'}
+  }>
         <h1>{props.heading} </h1>
         
-<div className="mb-3">
-  <textarea className="form-control"  value={text} onChange={handleOnChange} id="exampleFormControlTextarea1"  rows="3"></textarea>
+<div className="mb-3" >
+  <textarea className="form-control"  value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" style={
+    {backgroundColor: props.mode==='dark'?'gray':'white', color: props.mode==='dark'?'white':'black'},
+    {backgroundColor: props.grayMode==='gray'?'red':'white', color: props.grayMode==='gray'?'white':'white'}
+  }
+  rows="3"></textarea>
 </div> 
-<button className=' btn btn-primary mx-1' onClick={handleUpClick}>Convert to Uppercase</button>
-<button className='btn btn-primary mx-1' onClick={handleLoClick}>Convert to Uppercase</button>
+<button className=' btn btn-primary mx-1' onClick={handleUpClick}  style={
+    {backgroundColor: props.grayMode==='gray'?'red':'blue', color: props.grayMode==='dark'?'white':'white'}}>Convert to Uppercase</button>
+<button className='btn btn-primary mx-1' onClick={handleLoClick}   style={
+    {backgroundColor: props.grayMode==='gray'?'red':'blue', color: props.grayMode==='dark'?'white':'white'}}>Convert to Lowercase</button>
 
-<button className='btn btn-primary mx-1' onClick={handleClearClick}>Clear</button>
+<button className='btn btn-primary mx-1' onClick={handleClearClick}   style={
+    {backgroundColor: props.grayMode==='gray'?'red':'blue', color: props.grayMode==='dark'?'white':'white'}}>Clear</button>
 
+<button className='btn btn-primary mx-1' onClick={handleCopyClick}   style={
+    {backgroundColor: props.grayMode==='gray'?'red':'blue', color: props.grayMode==='dark'?'white':'white'}}>copy</button>
+
+<button className='btn btn-primary mx-1' onClick={handleExtraSpaceClick}   style={
+    {backgroundColor: props.grayMode==='gray'?'red':'blue', color: props.grayMode==='dark'?'white':'white'}}>Remove Extra Space</button>
 
     </div>
   
 
-    <div className="container">
+    <div className="container" style={{ color: props.mode==='dark'?'white':'#042743'},
+        {backgroundColor: props.grayMode==='gray'?'red':'white', color: props.grayMode==='gray'?'white':'black'}
+  }>
       <h1>Your Text Summary</h1>
       <p>{text.split(" ").length} words and {text.length} Characters</p>
       <p>{0.008 * text.split(" ").length }  Mintues Read</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text.length>0?text: "Enter something in the textbox above to preview it here" }</p>
      
     </div>
     </>
